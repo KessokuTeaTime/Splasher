202267c1353183d3d8178731b23a4183a775f8c5
KrLite <68179735+KrLite@users.noreply.github.com>
2022-12-21 22:26:14 +0800

    cleanup

---
+++ src/main/java/net/krlite/splasher/Splasher.java
@@ -0,0 +1,13 @@
+package net.krlite.splasher;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Splasher implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("splasher");
+
+	@Override
+	public void onInitialize() {
+	}
+}
