c6de4938add769a24f6b415f8ca7987fc3f261de
TexTrue <3140846162@qq.com>
2023-08-27 07:55:33 +0800

    port to arch (WIP)

--- forge/src/main/java/net/krlite/splasher/forge/SplasherClientForge.java
+++ forge/src/main/java/net/krlite/splasher/forge/SplasherClientForge.java
@@ -6,15 +6,19 @@ import dev.architectury.platform.forge.EventBuses;
 import net.krlite.splasher.Splasher;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Splasher.ID)
 public class SplasherClientForge {
     public SplasherClientForge() {
         EventBuses.registerModEventBus(Splasher.ID, FMLJavaModLoadingContext.get().getModEventBus());
         IEventBus modEventBus = EventBuses.getModEventBus(Splasher.ID).get();
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 
         modEventBus.addListener(this::onInitializeClient);
     }
