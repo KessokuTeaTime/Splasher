fa871ee06cad90aaaac45c59873c9ec31c28ae21
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-08-27 21:46:11 +0800

    port to arch

---
+++ forge/src/main/java/net/krlite/splasher/forge/SplasherClientForge.java
@@ -0,0 +1,41 @@
+package net.krlite.splasher.forge;
+
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.client.ClientGuiEvent;
+import dev.architectury.event.events.client.ClientScreenInputEvent;
+import dev.architectury.platform.forge.EventBuses;
+import net.krlite.splasher.Splasher;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Splasher.ID)
+public class SplasherClientForge {
+    public SplasherClientForge() {
+        // Submit our event bus to let architectury register our content on the right time
+        EventBuses.registerModEventBus(Splasher.ID, FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus modEventBus = EventBuses.getModEventBus(Splasher.ID).get();
+
+        modEventBus.addListener(this::onInitializeClient);
+    }
+
+    public void onInitializeClient(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            Splasher.onInitClient();
+
+            ClientGuiEvent.INIT_POST.register((screen, screenAccess) -> {
+                if (screen instanceof TitleScreen) {
+                    ClientScreenInputEvent.MOUSE_CLICKED_POST.register((client, currentScreen, mouseX, mouseY, button) -> {
+                        if (Splasher.isMouseHovering(screenAccess.getScreen().width, mouseX, mouseY) && Splasher.CONFIG.randomRate.onClick()) {
+                            Splasher.push();
+                            Splasher.playClickingSound();
+                        }
+                        return EventResult.pass();
+                    });
+                }
+            });
+        });
+    }
+}
