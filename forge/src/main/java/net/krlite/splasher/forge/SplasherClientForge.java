a60524a61e0e9d3a0d5e9befff21a911a7c6c907
TexTrue <3140846162@qq.com>
2023-09-01 10:54:21 +0800

    Re-linkage with Bounced

--- forge/src/main/java/net/krlite/splasher/forge/SplasherClientForge.java
+++ forge/src/main/java/net/krlite/splasher/forge/SplasherClientForge.java
@@ -4,6 +4,7 @@ import dev.architectury.event.EventResult;
 import dev.architectury.event.events.client.ClientGuiEvent;
 import dev.architectury.event.events.client.ClientScreenInputEvent;
 import dev.architectury.platform.forge.EventBuses;
+import net.krlite.bounced.Bounced;
 import net.krlite.splasher.Splasher;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,35 +18,31 @@ import net.minecraftforge.network.NetworkConstants;
 @Mod(Splasher.ID)
 public class SplasherClientForge {
     public SplasherClientForge() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         EventBuses.registerModEventBus(Splasher.ID, FMLJavaModLoadingContext.get().getModEventBus());
-
         IEventBus modEventBus = EventBuses.getModEventBus(Splasher.ID).get();
-        ModLoadingContext.get().registerExtensionPoint(
-                IExtensionPoint.DisplayTest.class,
-                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true)
-        );
 
         modEventBus.addListener(this::onInitializeClient);
     }
 
     public void onInitializeClient(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            Splasher.onInitClient();
+        Splasher.onInitClient();
 
-            ClientGuiEvent.INIT_POST.register((screen, screenAccess) -> {
-                if (screen instanceof TitleScreen) {
-                    ClientScreenInputEvent.MOUSE_CLICKED_POST.register((client, currentScreen, mouseX, mouseY, button) -> {
-                        if (
-                                Splasher.isMouseHovering(screenAccess.getScreen().width, mouseX, mouseY)
-                                        && Splasher.CONFIG.randomRate.onClick()
-                        ) {
-                            Splasher.push();
-                            Splasher.playClickingSound();
-                        }
-                        return EventResult.pass();
-                    });
-                }
-            });
+        ClientGuiEvent.INIT_POST.register((screen, screenAccess) -> {
+            if (screen instanceof TitleScreen) {
+                ClientScreenInputEvent.MOUSE_CLICKED_POST.register((client, currentScreen, mouseX, mouseY, button) -> {
+                    if (Splasher.isBouncedLoaded) {
+                        // Linkage with Bounced
+                        mouseY -= Bounced.primaryPos();
+                    }
+
+                    if (Splasher.isMouseHovering(screenAccess.getScreen().width, mouseX, mouseY) && Splasher.CONFIG.randomRate.onClick()) {
+                        Splasher.push();
+                        Splasher.playClickingSound();
+                    }
+                    return EventResult.pass();
+                });
+            }
         });
     }
 }
