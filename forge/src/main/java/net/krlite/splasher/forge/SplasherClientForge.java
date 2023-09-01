192b1724a8a88206819357f82e8099de693af38d
TexTrue <3140846162@qq.com>
2023-09-01 15:01:56 +0800

    Re-linkage with Bounced

--- forge/src/main/java/net/krlite/splasher/forge/SplasherClientForge.java
+++ forge/src/main/java/net/krlite/splasher/forge/SplasherClientForge.java
@@ -5,6 +5,7 @@ import dev.architectury.event.events.client.ClientGuiEvent;
 import dev.architectury.event.events.client.ClientScreenInputEvent;
 import dev.architectury.platform.forge.EventBuses;
 
+import net.krlite.bounced.Bounced;
 import net.krlite.splasher.Splasher;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -27,7 +28,13 @@ public class SplasherClientForge {
         ClientGuiEvent.INIT_POST.register((screen, screenAccess) -> {
             if (screen instanceof TitleScreen) {
                 ClientScreenInputEvent.MOUSE_CLICKED_POST.register((client, currentScreen, mouseX, mouseY, button) -> {
-                    if (Splasher.isMouseHovering(screenAccess.getScreen().width, mouseX, mouseY) && Splasher.CONFIG.randomRate.onClick()) {
+                    double scaledWidth = screenAccess.getScreen().width;
+                    if (Splasher.isBouncedLoaded) {
+                        // Linkage with Bounced
+                        mouseY -= Bounced.primaryPos();
+                    }
+
+                    if (Splasher.isMouseHovering(scaledWidth, mouseX, mouseY) && Splasher.CONFIG.randomRate.onClick()) {
                         Splasher.push();
                         Splasher.playClickingSound();
                     }
