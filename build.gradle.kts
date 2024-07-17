ebc662797adb112b1af7c2d4f28172fc45e890c2
KrLite <68179735+KrLite@users.noreply.github.com>
2024-07-18 07:10:37 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ publisher {
 
 	versionType.set("release")
 	projectVersion.set(project.version.toString())
-	gameVersions.set(listOf("1.20.3", "1.20.4"))
+	gameVersions.set(listOf("1.20.3", "1.20.4", "1.20.5", "1.20.6"))
 	loaders.set(listOf("fabric", "quilt"))
 	curseEnvironment.set("client")
 
